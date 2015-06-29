7c98bd9c2fafd349e698fec42ea0c22ab65a2f90
KimSeokhwan <shk1447@n3n.co.kr>
2015-06-29 19:53:23 +0900

    게시판 밑작업중

---
+++ client/views/editcontents.js
@@ -0,0 +1,18 @@
+/**
+ * author : Seokhwan Kim <shk1447@n3n.co.kr>
+ */
+
+if (Meteor.isClient) {
+    Template.editcontents.onCreated(function () {
+    });
+
+    Template.editcontents.onRendered(function () {
+    });
+
+    Template.editcontents.onDestroyed(function () {
+    });
+
+    Template.editcontents.helpers({});
+
+    Template.editcontents.events({});
+}
